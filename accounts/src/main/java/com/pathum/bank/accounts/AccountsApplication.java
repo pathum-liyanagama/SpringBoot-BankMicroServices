3298a00d6a1792df25ebb0c53a8666f6a9beea4c
Pathum Liyanagama <pathumpml.dev@gmail.com>
2023-11-25 23:32:40 +0800

    accounts - Remove null values in Response

--- accounts/src/main/java/com/pathum/bank/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/pathum/bank/accounts/AccountsApplication.java
@@ -1,13 +1,22 @@
 package com.pathum.bank.accounts;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccountsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+	// To remove null values in Response coming from DTO objects
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
 	}
 
 }
