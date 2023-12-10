8a654afac9596879f0b05d28a216c4a9c1132d9c
Pathum Liyanagama <pathumpml.dev@gmail.com>
2023-12-10 13:02:59 +0800

    accounts - Add audit fields created by, created date and updated date automatically

--- accounts/src/main/java/com/pathum/bank/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/pathum/bank/accounts/AccountsApplication.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication {
 
 	public static void main(String[] args) {
